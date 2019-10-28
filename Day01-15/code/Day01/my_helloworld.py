b1a22209fe5cf076f27632c4aab0ef3aa46e92dc
QuYuan891211 <quyuan891211@163.com>
2019-10-28 21:34:23 +0800

    Day1

---
+++ Day01-15/code/Day01/my_helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+# @Time    : 2019/10/28 21:28
+# @Author  : yq
+# @Site    : 
+# @File    : my_helloworld.py
+# @Software: PyCharm
+print('hello world!')
+print('hello', 'world', sep=':', end=' 666')
+# \n可以让本行接上一行
+print('hello', 'world', end='!\n')
