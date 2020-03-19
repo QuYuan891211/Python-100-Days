fe10a80f223c4c4bf1d4415e4368b6549d682c44
QuYuan <quyuan891211@163.com>
2020-03-19 23:00:17 +0800

    D1，D2

---
+++ Day01-15/code/Day01/my/helloWorld.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# Author    : Qu Yuan <quyuankenny@163.com >
+# Datetime  : 2020/3/19 16:44
+# User      : qq
+# Product   : PyCharm
+# Project   : Python-100-Days
+# File      : helloWorld.py
+# explain   : 文件说明
+
+print('hello world')
